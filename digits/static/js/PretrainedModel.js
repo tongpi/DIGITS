cc72ed857cf708f52dfe039c2b7909e05ba8bfca
sunxingba <sunxingba@e-u.cn>
2019-02-25 10:25:15 +0800

    test js file zh_CN

--- digits/static/js/PretrainedModel.js
+++ digits/static/js/PretrainedModel.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2016-2017, NVIDIA CORPORATION.  All rights reserved.
 var gt = new Gettext({domain: 'digits'});
-var _ = function(msgid) { return gt.gettext(msgid); };
+var get_text = function(msgid) { return gt.gettext(msgid); };
 
 
 var PretrainedModel = function(params) {
@@ -36,8 +36,8 @@ var PretrainedModel = function(params) {
   self.resize_modes = [
     {text: 'Squash', value: 'squash'},
     {text: 'Crop', value: 'crop'},
-    {text: _('Fill'), value: 'fill'},
-    {text: _('Half Crop, Half Fill'), value: 'half_crop'}
+    {text: get_text('Fill'), value: 'fill'},
+    {text: get_text('Half Crop, Half Fill'), value: 'half_crop'}
   ];
 
   self.frameworkChanged = function() {
@@ -114,7 +114,7 @@ var PretrainedModel = function(params) {
 
   self.well = function(params) {
     var props = _.extend({
-      text: _('Upload Tar or Zip Archive'),
+      text: get_text('Upload Tar or Zip Archive'),
       class: '',
       state: 'primary'
     },params);
