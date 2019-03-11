9c44ca0b87ff59d376a1b51cd9020cb424de3e32
tinyEnd <41897529+tinyEnd@users.noreply.github.com>
2019-03-11 13:34:16 +0800

    测试

--- digits/config/gpu_list.py
+++ digits/config/gpu_list.py
@@ -4,7 +4,5 @@ from __future__ import absolute_import
 from . import option_list
 import digits.device_query
 
-print('*' * 30)
-print(digits.device_query.get_devices())
-print('*' * 30)
+
 option_list['gpu_list'] = ','.join([str(x) for x in xrange(len(digits.device_query.get_devices()))])
