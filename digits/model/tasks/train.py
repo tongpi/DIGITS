555b3f3ed6300d42b21fb515cdbc4f5aa6008bf3
tinyEnd <41897529+tinyEnd@users.noreply.github.com>
2019-03-26 15:11:41 +0800

    学习率

--- digits/model/tasks/train.py
+++ digits/model/tasks/train.py
@@ -513,7 +513,7 @@ class TrainTask(Task):
                 'lr': 'epoch'
             },
             'names': {
-                'lr': _('Learning Rate')
+                'lr': 'LLLLLLLLLL'
             },
         }
 
